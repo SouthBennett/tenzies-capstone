14b165cfbffdf1e393d999d0932c2ef86138ae3e
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-13 16:50:17 -0700

    implemented Die component and rendered it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-export default function App() {
+import Die from './components/Die'
+
+export default function App() {  
+
   return (
-    <main></main>
+    <main>
+      <div className="dice-container">
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+        <Die value={5}/>
+        <Die value={6}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={1}/>
+      </div>
+    </main>
+    
   )
 }
