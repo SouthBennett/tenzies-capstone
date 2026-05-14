7b57784da9093380b47ccafff64b7d202cbc767f
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-13 17:59:26 -0700

    Tenzies: Generate 10 random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+/* eslint-disable react-hooks/purity */
 import Die from './components/Die'
 
 export default function App() {  
+
+  function generateAllNewDice (){
+    const randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+      randomNumbers.push(Math.ceil(Math.random() * 6))
+    }
+    return (
+      randomNumbers
+    )
+  }
+  console.log(generateAllNewDice())
 
   return (
     <main>
