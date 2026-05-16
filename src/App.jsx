68a658b8821ae380362c5ff9a49c6e188942bd43
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-15 20:30:17 -0700

    Tenzies: Endgame - part 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Die from './components/Die'
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {  
 
@@ -70,6 +71,7 @@ export default function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
