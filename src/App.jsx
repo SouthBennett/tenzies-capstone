02cd166e4944c67703caec6c4433285662effc63
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-15 19:44:55 -0700

    Tenzies: Hold dice - part 3

--- src/App.jsx
+++ src/App.jsx
@@ -50,11 +50,23 @@ export default function App() {
   })
 
   function rollDice() {
-    setDice(generateAllNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      if (die.isHeld === true) {
+        return {
+          ...die
+        }
+      }
+      return {
+        ...die, 
+        value: Math.ceil(Math.random() * 6)
+      }
+    }))
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
